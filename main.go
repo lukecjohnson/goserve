ac03e6a48bc903079faa597e9fc8fd5149a0ce03
Luke Johnson <luke.johnson9423@gmail.com>
2020-06-28 17:38:05 -0400

    clean url handling

--- main.go
+++ main.go
@@ -8,14 +8,28 @@ import (
   flag "github.com/spf13/pflag"
 )
 
+type Dir struct {
+  http.Dir
+}
+
+func (d Dir) Open(name string) (http.File, error) {
+  f, err := d.Dir.Open(name)
+  if os.IsNotExist(err) {
+    if f, err := d.Dir.Open(name + ".html"); err == nil {
+      return f, nil
+    }
+  }
+  return f, err
+}
+
 func main() {
   port := flag.StringP("port", "p", "8080", "Port to serve on")
   flag.Parse()
 
   arguments := flag.Args()
-	directory := arguments[0]
+  directory := arguments[0]
   
-  fs := http.FileServer(http.Dir(directory))
+  fs := http.FileServer(Dir{http.Dir(directory)})
   http.Handle("/", fs)
 
   fmt.Printf("Serving %s on port %s \n", directory, *port)
