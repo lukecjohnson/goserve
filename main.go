b0e1276d5c7b9fed9aca2f7f5e760ef9257ec6ac
Luke Johnson <luke.johnson9423@gmail.com>
2020-07-01 00:29:47 -0400

    add flag to open browser window when starting server

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"runtime"
 
 	flag "github.com/spf13/pflag"
 )
@@ -26,10 +29,31 @@ func (d dir) Open(name string) (http.File, error) {
 	return f, err
 }
 
+func openBrowser(url string) error {
+	var cmd string
+	var args []string
+
+	if runtime.GOOS == "darwin" {
+		cmd = "open"
+	} else if runtime.GOOS == "windows" {
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	} else if runtime.GOOS == "linux" {
+		cmd = "xdg-open"
+	} else {
+		return errors.New("Error: Unsupported platform")
+	}
+
+	args = append(args, url)
+
+	return exec.Command(cmd, args...).Start()
+}
+
 func main() {
 	port := flag.StringP("port", "p", "8080", "Port to serve on")
 	cert := flag.StringP("cert", "c", "", "Path to SSL certificate")
 	key := flag.StringP("key", "k", "", "Path to the SSL certificate's private key")
+	open := flag.BoolP("open", "o", false, "Open browser window")
 	version := flag.BoolP("version", "v", false, "Prints the current version of goserve")
 
 	flag.Parse()
@@ -49,9 +73,23 @@ func main() {
 	fs := http.FileServer(dir{http.Dir(directory)})
 	http.Handle("/", fs)
 
-	fmt.Printf("Serving %s on port %s \n", directory, *port)
+	secure := *cert != "" && *key != ""
 
-	if *cert != "" && *key != "" {
+	url := "http://localhost:" + *port
+	if secure {
+		url = "https://localhost:" + *port
+	}
+
+	if *open {
+		if err := openBrowser(url); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Printf("Serving %s at %s \n", directory, url)
+
+	if secure {
 		log.Fatal(http.ListenAndServeTLS("localhost:"+*port, *cert, *key, nil))
 	} else {
 		log.Fatal(http.ListenAndServe("localhost:"+*port, nil))
